23c1654d310421f82b88ca4bd87fd461c243bf60
juliocesar08102006 <juliocesar08102006@gmail.com>
2026-05-12 19:11:33 -0400

    base se datos  con instrumento añadido

--- base de datos EMMAE.sql
+++ base de datos EMMAE.sql
@@ -14,7 +14,8 @@ CREATE TABLE docente (
     id_docente INT AUTO_INCREMENT PRIMARY KEY,
     cedula INT NOT NULL,
     nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100) NOT NULL
+    apellido VARCHAR(100) NOT NULL,
+    especialidad varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ============================================================
@@ -28,3 +29,27 @@ CREATE TABLE estudiantes (
     instrumento VARCHAR(100) NOT NULL,
     piano_complementario BOOLEAN NOT NULL DEFAULT FALSE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ===========================================================================
+-- TABLA 3: instrumentos 
+-- ============================================================================
+USE EMMAE_BASEDEDATOS;
+
+CREATE TABLE IF NOT EXISTS instrumentos (
+  id_instrumento INT AUTO_INCREMENT PRIMARY KEY,
+  tipo_instrumento VARCHAR(100) NOT NULL,
+  instrumento_en_prestamo BOOLEAN NOT NULL DEFAULT FALSE,
+  estado VARCHAR(100) NOT NULL,
+  fecha_prestamo DATE NULL,
+  hora_prestamo TIME NULL,
+  fecha_limite DATE NULL,
+  hora_limite TIME NULL,
+  id_estudiante INT NULL,
+  id_docente INT NULL,
+  CONSTRAINT fk_instrumentos_estudiante FOREIGN KEY (id_estudiante)
+    REFERENCES estudiantes(id_estudiante) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT fk_instrumentos_docente FOREIGN KEY (id_docente)
+    REFERENCES docente(id_docente) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
